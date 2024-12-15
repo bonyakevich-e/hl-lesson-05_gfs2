76b35a3d0bd6729c41aa0cc60884ec89ed80d05d
itbn <ygbonyakevuch@gmail.com>
2024-12-15 21:58:14 +0300

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,28 @@
-
+# получаем id образа ubuntu 24.04
 data "yandex_compute_image" "ubuntu2404" {
-  # получаем id образа ubuntu 24.04
   family = "ubuntu-2404-lts-oslogin"
 }
 
+# получаем id дефолтной network
 data "yandex_vpc_network" "default" {
-  # получаем id дефолтной network
   name = "default"
 }
 
+# создаем дополнительный диск, который используется как iscsi share
 resource "yandex_compute_disk" "shared_disk" {
   name = "shared-disk"
   type = "network-hdd"
   size = "5"
 }
 
+# создаем подсеть
 resource "yandex_vpc_subnet" "subnet01" {
   name           = "subnet01"
   network_id     = data.yandex_vpc_network.default.network_id
   v4_cidr_blocks = ["10.16.0.0/24"]
 }
 
+# создаем сервер iscsi
 resource "yandex_compute_instance" "iscsi" {
   name     = "iscsi"
   hostname = "iscsi"
@@ -52,6 +54,7 @@ resource "yandex_compute_instance" "iscsi" {
   }
 }
 
+# создаем инстансы для будущего кластера
 resource "yandex_compute_instance" "cluster_instances" {
   count    = var.cluster_size
   name     = "${var.cluster_instance_name}${count.index + 1}"
@@ -82,6 +85,7 @@ resource "yandex_compute_instance" "cluster_instances" {
 
 }
 
+# создаем inventory файл для Ansible
 resource "local_file" "inventory" {
   filename        = "./hosts"
   file_permission = "0644"
@@ -97,7 +101,8 @@ ${vm.hostname} ansible_host=${vm.network_interface.0.nat_ip_address} ansible_ssh
 EOT
 }
 
-resource "local_file" "init_yml" {
+# создаем Ansible playbook
+resource "local_file" "playbook_yml" {
   filename        = "./playbook.yml"
   file_permission = "0644"
   content = templatefile("playbook.tmpl.yml", {
@@ -113,6 +118,7 @@ resource "local_file" "init_yml" {
   })
 }
 
+# создаем скрипт, который выполнит настройку
 resource "local_file" "setup_iscsi_target" {
   filename        = "./iscsi_target.bash"
   file_permission = "0644"
